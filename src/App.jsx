d2bb2fc71385dc61c54a57ec0bcb0044a93f8b24
Reymark <generaltony663@gmail.com>
2024-11-13 23:54:09 +0800

    hehe

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   return (
    <GlobalContext>
       <GoogleOAuthProvider clientId="795468337489-oqs3dg7q59raqliejpvu5h6nlc70apuf.apps.googleusercontent.com">
-        <Router basename='/Markies'>
+        <Router>
          <AppContent />
         </Router>
       </GoogleOAuthProvider>
