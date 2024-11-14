79706eb6bb57a2b66efcb5c039e090f29aef57c4
Reymark <generaltony663@gmail.com>
2024-11-14 12:03:45 +0800

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,27 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useLocation
-} from 'react-router-dom';
+  useLocation,
+} from "react-router-dom";
 
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Header from './components/Header/Header';
-import MainMenu from './pages/MainMenu';
-import Desert from './pages/Desert';
-import Beverages from './pages/Beverages';
-import Cart from './pages/Cart';
-import Vouchers from './pages/Vouchers';
-import Pending from './pages/Pending';
-import Profile from './pages/Profile';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GlobalContext } from './Context/GlobalContext';
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Header from "./components/Header/Header";
+import MainMenu from "./pages/MainMenu";
+import Desert from "./pages/Desert";
+import Beverages from "./pages/Beverages";
+import Cart from "./pages/Cart";
+import Vouchers from "./pages/Vouchers";
+import Pending from "./pages/Pending";
+import Profile from "./pages/Profile";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GlobalContext } from "./Context/GlobalContext";
 
 function App() {
   return (
     <GlobalContext>
       <GoogleOAuthProvider clientId="795468337489-oqs3dg7q59raqliejpvu5h6nlc70apuf.apps.googleusercontent.com">
-        <Router basename="/Markies"> {/* Set the basename here */}
+        <Router basename="/Markies">
           <AppContent />
         </Router>
       </GoogleOAuthProvider>
@@ -32,10 +32,10 @@ function App() {
 
 function AppContent() {
   const location = useLocation();
-  const isLoginPage = location.pathname === '/login';
+  const isLoginPage = location.pathname === "/login";
 
   return (
-    <> 
+    <>
       {!isLoginPage && <Header />}
       <Routes>
         <Route path="/login" element={<Login />} />
